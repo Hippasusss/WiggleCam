00d72a376fad166353ec4bf0b1d7ddab3803c9af
gjttt1 <dannyherbertgj@gmail.com>
2020-12-23 22:57:09 +0000

    added controller and started debuggin client

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ from vidgear.gears import NetGear
 import cv2
 
 #CONTROLLER
-SERVERADRESSES = { "172.19.181.1", "172.19.181.2", "172.19.181.3", "172.19.181.4"}
+SERVERADRESSES = [ "172.19.181.1", "172.19.181.2", "172.19.181.3", "172.19.181.4" ]
 PORT = "8000"
 
 class Client:
@@ -10,12 +10,19 @@ class Client:
 
     client = None
 
-    def runPreviewWindow(self, piNumber):
-        sendInstruction("preview", piNumber)
+    def startPreviewWindow(self, piNumber):
+        piNumber -= 1
+        #sendInstruction("preview", piNumber)
+
+        print("connecting to IP: {0}".format(SERVERADRESSES[piNumber]))
+        print("connecting on PORTP: {0}".format(PORT))
+
         self.client = NetGear(receive_mode = True, adress = SERVERADRESSES[piNumber], port = PORT)
 
+        print("Success!")
+
         while True:
-            frame = client.recv()
+            frame = self.client.recv()
             if frame is None:
                 break
 
@@ -31,12 +38,15 @@ class Client:
     def sendInstruction(self, instruction, piNumber):
 
         if instruction is "preview":
+            hello=True
             #send the preview instruction to the pi
 
         if instruction is "stop":
+            hello=True
             # stop the preview window on the pi
         # tell a server to take a photo
 
         if instruction is "photo":
+            hello=True
             # tell the server to stop everythin and take a photo at a set time
 
---
+++ controller.py
@@ -0,0 +1,8 @@
+import client 
+import time
+
+controller = client.Client()
+
+controller.startPreviewWindow(1)
+
+time.sleep(20)
