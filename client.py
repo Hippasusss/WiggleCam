e5cdc09e0e8d6d0b9c4f9515458ccf00d23a99f3
gjttt1 <dannyherbertgj@gmail.com>
2020-12-24 23:58:50 +0000

    refactored out messager into own class

--- client.py
+++ client.py
@@ -9,20 +9,5 @@ class Client:
 
     previewWindow = preview.Preview(receiveMode = True)
 
-    # Sends an instruciton to the desired pi and waits for a response to confime recipt of message
-    def sendInstruction(self, instruction, piNumber):
-
-        if instruction is "preview":
-            hello=True
-            #send the preview instruction to the pi
-
-        if instruction is "stop":
-            hello=True
-            # stop the preview window on the pi
-        # tell a server to take a photo
-
-        if instruction is "photo":
-            hello=True
-            # tell the server to stop everythin and take a photo at a set time
 
 
--- server.py
+++ server.py
@@ -20,33 +20,4 @@ class Server:
 
     previewWindow = preview.Preview(receiveMode = False)
 
-    monitorThread = None
-    monitoring = False
-
-    def startMessageMonitor(self):
-
-        self.monitoring = True
-        if self.monitorThread is None:
-            self.monitorThread = threading.Thread(target=self.messageMonitor)
-            self.monitorThread.start()
-
-        print("Started monitoring for messages...")
-
-    def stopMessageMonitor(self):
-        self.monitoring = False
-        if self.monitorThread is not None:
-            self.monitorThread.join()
-            self.monitorThread = None
-
-        print("Stopped Monitoring for messages...")
-
-    def messageMonitor(self):
-        while self.monitoring:
-            time.sleep(0.5)
-            print("monitoring")
-
-
-    # takes a photo and sends it back to the client 
-    def takePhoto(self):
-        print("Took Photo...")
 
