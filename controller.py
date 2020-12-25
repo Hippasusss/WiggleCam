837b9e84ed79460302e55f0033b6507ad558e8da
Hippasusss <dannyherbertgj@gmail.com>
2020-12-25 22:43:52 +0000

    -

--- controller.py
+++ controller.py
@@ -2,11 +2,11 @@ import client
 import time
 
 SERVERADRESSES = [ "172.19.181.1", "172.19.181.2", "172.19.181.3", "172.19.181.4" ]
-PREVIEWPORT = "8000"
+PREVIEWPORT = "5454"
 
 controller = client.Client()
 
-controller.previewWindow.startPreview(SERVERADRESSES[1], PREVIEWPORT)
+controller.previewWindow.startPreview("127.0.0.1", PREVIEWPORT)
 
 controller.previewWindow.waitForTermination()
 
