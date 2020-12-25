ebd2258f74f0b308a8de10e2716de3270de9e725
Hippasusss <dannyherbertgj@gmail.com>
2020-12-25 23:04:28 +0000

    -

--- controller.py
+++ controller.py
@@ -4,6 +4,9 @@ import time
 #SERVERADRESSES = [ "172.19.181.1", "172.19.181.2", "172.19.181.3", "172.19.181.4" ]
 PREVIEWPORT = "5454"
 
+ADRESS = "172.19.181.254"
+#ADRESS = "172.19.180.254"
+
 controller = client.Client()
 
 controller.previewWindow.startPreview(ADRESS, PREVIEWPORT)
