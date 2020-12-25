196eb6995ea3c31f70cab084a4a9d5e6f4c32040
Hippasusss <dannyherbertgj@gmail.com>
2020-12-25 18:26:04 +0000

    fixed errors on camera server pi

--- cameraModule.py
+++ cameraModule.py
@@ -4,13 +4,13 @@ import time
 # MAINCOMP   = "192.168.1.160"
 # CONTROLLER = "192.168.1.86"
 CLIENTADRESS = "192.168.1.86"
-PREVIEWPORT = "8001"
+PREVIEWPORT = "8000"
 
 module = server.Server()
 
 module.previewWindow.startPreview(CLIENTADRESS, PREVIEWPORT)
 
-module.waitForTermination()
+module.previewWindow.waitForTermination()
 
 
 
