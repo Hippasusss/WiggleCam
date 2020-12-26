5e1941a16d4df9b4504a242013b2ac165b3ed400
Hippasusss <dannyherbertgj@gmail.com>
2020-12-26 17:04:40 +0000

    -

--- cameraModule.py
+++ cameraModule.py
@@ -2,14 +2,12 @@ import server
 import time
 
 CLIENTADRESS = "172.19.181.254"
-# CLIENTADRESS = "172.19.180.254"
-# CLIENTADRESS = "192.168.1.86"
-# CLIENTADRESS = "192.168.1.86"
+#CLIENTADRESS = "192.168.1.86"
 PREVIEWPORT = "5454"
 
 module = server.Server()
 
-module.previewWindow.startPreview(CLENTADRESS, PREVIEWPORT)
+module.previewWindow.startPreview(CLIENTADRESS, PREVIEWPORT)
 
 module.previewWindow.waitForTermination()
 
