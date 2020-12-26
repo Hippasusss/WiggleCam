8378a5fe93b4d461d9ef022531016365db0ccc4c
Hippasusss <dannyherbertgj@gmail.com>
2020-12-26 22:33:59 +0000

    first connection with client

--- cameraModule.py
+++ cameraModule.py
@@ -1,9 +1,14 @@
 import server
 import time
 
+"""
 CLIENTADRESS = "172.19.181.254"
-#CLIENTADRESS = "192.168.1.86"
-PREVIEWPORT = "5454"
+CLIENTADRESS = "172.19.180.254"
+CLIENTADRESS = "192.168.1.86"
+"""
+
+CLIENTADRESS = "172.19.181.254"
+PREVIEWPORT = "5555"
 
 module = server.Server()
 
