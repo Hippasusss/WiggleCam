918f8609d0c9dbd94c07e5106b86f174c2629b76
Hippasusss <dannyherbertgj@gmail.com>
2020-12-31 23:47:47 +0000

    updated install file

--- installDep.sh
+++ installDep.sh
@@ -1,5 +1,9 @@
 sudo apt-get update -y; sudo apt-get upgrade -y;
 
-sudo apt-get install python3-opencv python3-pip -y; pip3 install vidgear opencv-python picamera;
+sudo apt-get install python3-opencv python3-pip -y; 
+pip3 install vidgear opencv-python picamera;
+
+pip3 install opencv-contrib-python; 
+sudo apt-get install -y libatlas-base-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev  libqtgui4  libqt4-test;
 
 sudo raspi-config nonint do_camera 0;
