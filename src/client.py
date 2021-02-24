3cfe51b77ed3a25c9e2ea8c8773abe1e66101e8b
Hippasusss <dannyherbertgj@gmail.com>
2021-02-24 11:10:35 +0000

    adding threaded input

--- src/client.py
+++ src/client.py
@@ -1,4 +1,5 @@
 from vidgear.gears import NetGear 
+from getkey import getkey, keys
 import threading
 import cv2
 import preview
@@ -6,7 +7,26 @@ import preview
 #CONTROLLER
 
 class Client:
+
+    PORT = None
+    ADDRESS = None
+
     previewWindow = preview.Preview(receiveMode = True)
 
+    running = True
+
+    def __init__(ADDRESS, PORT):
+        self.ADDRESS = ADRESS
+        self.PORT = PORT
+
+    def startClient():
+        while(running):
+
+    def endClient():
+        running = False
+        
+class Input:
+    def __init__():
+        
 
 
