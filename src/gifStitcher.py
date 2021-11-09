b9b68cc7f2dae87e992e28b78cccce0ed0abfc53
Hippasusss <dannyherbertgj@gmail.com>
2021-11-09 22:14:39 +0000

    gif stitcher rework

--- src/gifStitcher.py
+++ src/gifStitcher.py
@@ -1,40 +1,40 @@
 from pathlib import Path
-import imageio
+from datetime import datetime
 import numpy
 import PIL
 import os
-from datetime import datetime
+import cv2
 
 def savePhotos(listOfPhotos):
-    filetype = ".png"
-    now=datetime.now().strftime("%d-%m-%y %H%M%S")
+    imageType = ".png"
+    videoType = ".mp4"
+    now = datetime.now().strftime("%d-%m-%y %H%M%S")
     path = f"{str(Path.home())}/photos/{now}"
     images = []
+    imageSize = (4064, 3040)
 
     # save photos as png images
     for i, photo in enumerate(listOfPhotos):
-        filePath = f"{path}/IMG{i+1} {now}{filetype}"
+        filePath = f"{path}/IMG{i+1} {now}{imageType}"
         os.makedirs(os.path.dirname(filePath), exist_ok=True)
         size = len(photo)
-        image = PIL.Image.frombytes('RGB', (4064, 3040), bytes(photo))
+        image = PIL.Image.frombytes('RGB', imageSize, bytes(photo))
         images.append(image)
         image.save(filePath)
         print(f"saved at: {filePath}")
 
-    # save images as a wiggle video 
+    ## save images as a wiggle video 
     numPhotos = len(listOfPhotos)
     numLoops = 5
     arrangement = []
+    # save the order of the images into an array
+    codec = cv2.cv.CV_FOURCC(*'H264')
+    video = cv2.VideoWriter(f"{path}/VID {now}{videoType}", codec, 6, imageSize)
     for j in range(numLoops):
         for i in range((numPhotos * 2) - 2):
-            currentPhoto = abs(numPhotos - (i+1))
-            print(f"added: {currentPhoto}")
-            arrangement.append(images[currentPhoto])
-    writer = imageio.get_writer(f"{path}/VID {now}.mp4", fps = 6)
-
-    for image in arrangement:
-        writer.append_data(numpy.asarray(image))
-    writer.close()
+            currentPhotoIndex = abs(numPhotos - (i+1))
+            video.write(numpy.frombytes((images[currentPhotoIndex])))
+    video.release()
 
 
 def stitch(listOfPhotos, name):
